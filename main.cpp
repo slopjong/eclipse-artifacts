c6bfe43d639bee18f6aa5a0a7aec08366b7a0b10
slopjong <slopjong>
2012-07-05 02:40:15 +0200

    Reading the XML directly from a zip file. !xmlpatterns not working!
    The xmlpatterns module ends up with a network timeout as follows:
    
    Error FODC0002 in tag:trolltech.com,2007:QtXmlPatterns:QIODeviceVariable:inputDocument: Network timeout.
    
    QuaZipFile is a QIODevice and reading its data also works.

--- main.cpp
+++ main.cpp
@@ -14,71 +14,95 @@
 #include <cstdlib>
 #include <typeinfo>
 
+#include "quazip.h"
+#include "quazipfile.h"
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     QTextStream cout(stdout, QIODevice::WriteOnly);
-    QFile sourceDocument;
 
-    QString file = "resources/feature.xml";
+    QuaZip zipFile("resources/com.onpositive.gae.appwrench_1.5.0.jar");
+    zipFile.open(QuaZip::mdUnzip);
+    QuaZipFileInfo info;
+    QuaZipFile aFile(&zipFile);
 
-    sourceDocument.setFileName(file);
+    bool more = zipFile.goToFirstFile();
 
-    if(sourceDocument.open(QIODevice::ReadOnly))
+    if(!more)
     {
-        QXmlQuery query(QXmlQuery::XPath20);
-        query.bindVariable("inputDocument", &sourceDocument);
-        query.setQuery("doc($inputDocument)/feature/plugin");
+        cout << "The jar file doesn't contain any files." << endl;
+        exit(1);
+    }
 
-        if (!query.isValid())
+    while(more)
+    {
+        zipFile.getCurrentFileInfo(&info);
+        aFile.open(QIODevice::ReadOnly);
+        QString name = aFile.getActualFileName();
+
+        if(name == "feature.xml")
+            break;
+
+        more = zipFile.goToNextFile();
+        aFile.close();
+    }
+
+    QIODevice * sourceDocument = &aFile;
+
+    QXmlQuery query(QXmlQuery::XPath20);
+    query.bindVariable("inputDocument", sourceDocument);
+    query.setQuery("doc($inputDocument)/feature/plugin");
+
+    if (!query.isValid())
+    {
+        cout << "query valid" << endl;
+        exit(1);
+    }
+
+    QXmlResultItems results;
+    query.evaluateTo(&results);
+
+    QXmlItem item(results.next());
+    while(!item.isNull())
+    {
+        if (item.isNull())
         {
-            cout << "query valid" << endl;
+            cout << "query is null" << endl;
+            exit(1);
+        }
+        else if( item.isNode())
+        {
+            cout << "item is node" << endl;
+        }
+        else if( item.isAtomicValue())
+        {
+            cout << "item is atomic" << endl;
+        }
+
+        QXmlQuery tmpQuery;
+
+        tmpQuery.bindVariable("featureNode", item);
+        tmpQuery.setQuery("$featureNode/@id/string()");
+
+        if (!tmpQuery.isValid())
+        {
+            cout << "tmpquery not valid" << endl;
             exit(1);
         }
 
-        QXmlResultItems results;
-        query.evaluateTo(&results);
+        QStringList tmpResults;
+        if(tmpQuery.evaluateTo(&tmpResults))
+            cout << tmpResults.first() << endl;
+        else
+            cout << "Could not evaluate the query";
 
-        QXmlItem item(results.next());
-        while(!item.isNull())
-        {
-            if (item.isNull())
-            {
-                cout << "query is null" << endl;
-                exit(1);
-            }
-            else if( item.isNode())
-            {
-                cout << "item is node" << endl;
-            }
-            else if( item.isAtomicValue())
-            {
-                cout << "item is atomic" << endl;
-            }
-
-            QXmlQuery tmpQuery;
-
-            tmpQuery.bindVariable("featureNode", item);
-            tmpQuery.setQuery("$featureNode/@id/string()");
-
-            if (!tmpQuery.isValid())
-            {
-                cout << "tmpquery not valid" << endl;
-                exit(1);
-            }
-
-            QStringList tmpResults;
-            if(tmpQuery.evaluateTo(&tmpResults))
-                cout << tmpResults.first() << endl;
-            else
-                cout << "Could not evaluate the query";
-
-            item = results.next();
-        }
+        item = results.next();
     }
-    else
-        cout << "Could not read the XML document" << endl;
+
+    sourceDocument->close();
 
     exit(0);
     return a.exec();
